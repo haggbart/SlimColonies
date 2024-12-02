a8346fd5ea0973a21c8c1844f038d8064f8d4625
Raycoms <ray.neiheiser@gmail.com>
2024-12-02 13:55:55 +0100

    hotfix 10498

--- src/main/java/com/minecolonies/core/entity/ai/workers/guard/AbstractEntityAIGuard.java
+++ src/main/java/com/minecolonies/core/entity/ai/workers/guard/AbstractEntityAIGuard.java
@@ -521,7 +521,7 @@ public abstract class AbstractEntityAIGuard<J extends AbstractJobGuard<J>, B ext
 
                 if (currentPatrolPoint != null)
                 {
-                    setNextPatrolTargetAndMove(currentPatrolPoint);
+                    setNextPatrolTarget(currentPatrolPoint);
                 }
             }
         }
@@ -562,11 +562,11 @@ public abstract class AbstractEntityAIGuard<J extends AbstractJobGuard<J>, B ext
                     final MinerLevel level = buildingMiner.getFirstModuleOccurance(MinerLevelManagementModule.class).getCurrentLevel();
                     if (level == null)
                     {
-                        setNextPatrolTargetAndMove(buildingMiner.getPosition());
+                        setNextPatrolTarget(buildingMiner.getPosition());
                     }
                     else
                     {
-                        setNextPatrolTargetAndMove(level.getRandomCompletedNode(buildingMiner));
+                        setNextPatrolTarget(level.getRandomCompletedNode(buildingMiner));
                     }
                 }
                 else
@@ -589,7 +589,7 @@ public abstract class AbstractEntityAIGuard<J extends AbstractJobGuard<J>, B ext
      */
     public void setNextPatrolTargetAndMove(final BlockPos target)
     {
-        currentPatrolPoint = target;
+        setNextPatrolTarget(target);
         registerTarget(new AIOneTimeEventTarget(() ->
         {
             if (getState() == CombatAIStates.NO_TARGET)
@@ -598,6 +598,16 @@ public abstract class AbstractEntityAIGuard<J extends AbstractJobGuard<J>, B ext
             }
             return getState();
         }));
+    }
+
+    /**
+     * Sets the next patrol target.
+     *
+     * @param target the next patrol target.
+     */
+    private void setNextPatrolTarget(final BlockPos target)
+    {
+        currentPatrolPoint = target;
     }
 
     /**
