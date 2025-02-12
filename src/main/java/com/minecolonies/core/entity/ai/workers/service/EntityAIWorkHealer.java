4eccfc389f4bdd3341ff8796b06917577da16dc3
Raycoms <ray.neiheiser@gmail.com>
2025-02-12 11:36:52 +0100

    hotfix cure pre-request

--- src/main/java/com/minecolonies/core/entity/ai/workers/service/EntityAIWorkHealer.java
+++ src/main/java/com/minecolonies/core/entity/ai/workers/service/EntityAIWorkHealer.java
@@ -295,7 +295,7 @@ public class EntityAIWorkHealer extends AbstractEntityAIInteract<JobHealer, Buil
                 }
                 if (!hasRequest)
                 {
-                    worker.getCitizenData().createRequestAsync(new Stack(cure));
+                    worker.getCitizenData().createRequestAsync(new Stack(cure.getItemStack(), REQUEST_COUNT, 1));
                 }
             }
         }
