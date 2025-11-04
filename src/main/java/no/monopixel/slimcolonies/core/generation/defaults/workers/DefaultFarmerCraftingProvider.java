1e0b214c429d4d4e0ab43f55eb98d4f1b3e6c7ff
Roger Karlsen <rogerkarlsen@live.com>
2025-11-05 00:48:16 +0100

    Allow farmer to craft water buckets from empty buckets

--- src/main/java/no/monopixel/slimcolonies/core/generation/defaults/workers/DefaultFarmerCraftingProvider.java
+++ src/main/java/no/monopixel/slimcolonies/core/generation/defaults/workers/DefaultFarmerCraftingProvider.java
@@ -1,13 +1,13 @@
 package no.monopixel.slimcolonies.core.generation.defaults.workers;
 
-import no.monopixel.slimcolonies.api.colony.jobs.ModJobs;
-import no.monopixel.slimcolonies.api.crafting.ItemStorage;
-import no.monopixel.slimcolonies.api.equipment.ModEquipmentTypes;
-import no.monopixel.slimcolonies.core.generation.CustomRecipeProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import no.monopixel.slimcolonies.api.colony.jobs.ModJobs;
+import no.monopixel.slimcolonies.api.crafting.ItemStorage;
+import no.monopixel.slimcolonies.api.equipment.ModEquipmentTypes;
+import no.monopixel.slimcolonies.core.generation.CustomRecipeProvider;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -38,16 +38,21 @@ public class DefaultFarmerCraftingProvider extends CustomRecipeProvider
     protected void registerRecipes(@NotNull final Consumer<FinishedRecipe> consumer)
     {
         CustomRecipeBuilder.create(FARMER, MODULE_CRAFTING, "carved_pumpkin")
-                .inputs(List.of(new ItemStorage(new ItemStack(Items.PUMPKIN))))
-                .result(new ItemStack(Items.CARVED_PUMPKIN))
-                .requiredTool(ModEquipmentTypes.shears.get())
-                .build(consumer);
+            .inputs(List.of(new ItemStorage(new ItemStack(Items.PUMPKIN))))
+            .result(new ItemStack(Items.CARVED_PUMPKIN))
+            .requiredTool(ModEquipmentTypes.shears.get())
+            .build(consumer);
 
         CustomRecipeBuilder.create(FARMER, MODULE_CRAFTING, "mud")
-                .inputs(List.of(new ItemStorage(new ItemStack(Items.DIRT)),
-                        new ItemStorage(new ItemStack(Items.WATER_BUCKET))))
-                .result(new ItemStack(Items.MUD))
-                .lootTable(DefaultRecipeLootProvider.LOOT_TABLE_LARGE_BOTTLE)
-                .build(consumer);
+            .inputs(List.of(new ItemStorage(new ItemStack(Items.DIRT)),
+                new ItemStorage(new ItemStack(Items.WATER_BUCKET))))
+            .result(new ItemStack(Items.MUD))
+            .lootTable(DefaultRecipeLootProvider.LOOT_TABLE_LARGE_BOTTLE)
+            .build(consumer);
+
+        CustomRecipeBuilder.create(FARMER, MODULE_CRAFTING, "water_bucket")
+            .inputs(List.of(new ItemStorage(new ItemStack(Items.BUCKET))))
+            .result(new ItemStack(Items.WATER_BUCKET))
+            .build(consumer);
     }
 }
